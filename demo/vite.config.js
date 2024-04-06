fbad197d055b3d44c2c9c367605799ab7a1dcadf
AfifJ <afifjamhari.id@gmail.com>
2024-04-06 21:50:30 +0700

    Add manualChunks configuration to vite.config.js

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   assetsInclude: ["**/*.md"], // include Markdown files in the build
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          marked: ["marked"], // create a separate chunk for marked
+        },
+      },
+    },
+  },
 })
