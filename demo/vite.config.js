2f1666004abe9c7c1f4d2b8b4dec777105d70f1a
AfifJ <afifjamhari.id@gmail.com>
2024-04-07 23:43:29 +0700

    Update vite.config.js to remove mdx plugin and include react plugin

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["**/*.md"], // include Markdown files in the build
-  plugins: [
-    react(),
-    mdx({
-      jsxImportSource: "@mdx-js/react",
-    }),
-  ],
+  plugins: [react()],
 })
